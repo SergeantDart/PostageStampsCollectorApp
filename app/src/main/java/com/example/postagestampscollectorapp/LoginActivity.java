c56e3d7c4c0dd365f6f42c19663cf275e76d1af9
SergeantDart <dumitrascuantonio18@stud.ase.ro>
2020-11-22 20:11:30 +0200

    Alina

--- app/src/main/java/com/example/postagestampscollectorapp/LoginActivity.java
+++ app/src/main/java/com/example/postagestampscollectorapp/LoginActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,11 +18,38 @@ public class LoginActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText username=(EditText)findViewById(R.id.usernameEditText);
+        EditText password=(EditText)findViewById(R.id.passwordEditText) ;
+        Button blogin = (Button)findViewById(R.id.loginButton);
+
+        blogin.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String passWord=password.getText().toString();
+
+                if(user.equals("username") && passWord.equals("password")){
+                    Toast.makeText(getApplicationContext(), "Redirecting..",
+                            Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+                else if(user.equals("")&&passWord.equals("")){
+                    Toast.makeText(getApplicationContext(), "Please fill the required fields!",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Invalid username or password. Please try again!",
+                          Toast.LENGTH_LONG).show();
+               }
+
+            }
+        });
+
     }
 
-    public void openMainActivity(View v){
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-    }
 }
+
 
